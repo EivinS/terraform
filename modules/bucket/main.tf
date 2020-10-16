da03a3a3d69322dc9f0f8913b98f81c9257eea00
EivinS <71434505+EivinS@users.noreply.github.com>
2020-10-16 12:08:20 +0200

    Update main.tf
    added transfer-job and lifecycle rule to sink-bucket

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -12,6 +12,16 @@ resource "google_storage_bucket" "my-module-yesyes" {
   name          = "module-testing-bucket-${random_id.suffix2.hex}"
   location      = var.location
   force_destroy = var.force_destroy
+
+//Delete objects in sink-bucket after 5 days
+  lifecycle_rule {
+    condition {
+      age = 5
+    }
+    action {
+      type = "Delete"
+    }
+  }
 }
 
 # https://www.terraform.io/docs/providers/google/r/storage_bucket.html
@@ -52,8 +62,43 @@ resource "random_id" "suffix2" {
   byte_length = 2
 }
 
-resource "random_id" "suffix3" {
-  byte_length = 2
+resource "google_storage_transfer_job" "bucket-module" {
+  description = "transfering-data-bucket-module"
+  project     = var.gcp_project
+  status      = "ENABLED"
+  transfer_spec {
+    gcs_data_source {
+      bucket_name = google_storage_bucket.storage_bucket.name # Source bucket
+    }
+    gcs_data_sink {
+      bucket_name = google_storage_bucket.my-module-yesyes.name # Destination bucket
+    }
+    transfer_options {
+      overwrite_objects_already_existing_in_sink = false # Do not overwrite
+      delete_objects_from_source_after_transfer  = false # Do not delete source
+    }
+    object_conditions {
+      max_time_elapsed_since_last_modification = "604800s" # Skip files older than 1 week
+    }
+  }
+  schedule { # Run every day at 11:40
+    schedule_start_date {
+      year  = 2020
+      month = 10
+      day   = 17
+    }
+    schedule_end_date {
+      year  = 2020
+      month = 12
+      day   = 31
+    }
+    start_time_of_day {
+      hours   = 13
+      minutes = 00
+      seconds = 0
+      nanos   = 0
+    }
+  }
 }
 
 
